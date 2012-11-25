f5c7ebf2ea5aa54e52a0d02d568e4deecc9eff7b
Dominic Northey <dominicnorthey@gmail.com>
2012-11-25 12:00:44 +0000

    Added Upload Button

--- ObjectTracking/Form1.cs
+++ ObjectTracking/Form1.cs
@@ -15,5 +15,41 @@ namespace ObjectTracking
 		{
 			InitializeComponent();
 		}
+
+		/// <summary>
+		/// Based my upload function on this article.
+		/// 
+		/// http://voices.yahoo.com/how-create-browse-file-dialog-box-c-6141416.html
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void Upload(object sender, EventArgs e)
+		{
+			// Call a browse files dialog box
+
+			// Intialize variables
+
+			string strDirectory = "C:/Users/Dominic Northey/Documents/Work/Notes/Year 6/Sem 1/Computer Vision";
+
+			// Open the dialog box
+
+			OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+			openFileDialog1.InitialDirectory = strDirectory;
+
+			openFileDialog1.Filter = "Video Files (*.mp4, *.wma)|*.mp4, *.wma|All files (*.*)|*.*";
+			openFileDialog1.FilterIndex = 1;
+			openFileDialog1.Multiselect = false;
+			openFileDialog1.RestoreDirectory = true;
+
+			DialogResult Result = openFileDialog1.ShowDialog();
+
+			// Load the image to the load image picture box
+
+			if (Result == DialogResult.OK)
+			{
+				//openFileDialog1.FileName
+			}
+		}
 	}
 }
