75c30c7dd3dd7b34e7f9fde8bc4ad728ed94046b
Dominic Northey <dominicnorthey@gmail.com>
2012-11-30 00:03:59 +0000

    Fixed to update image on an event fire

--- ObjectTracking/Form1.Designer.cs
+++ ObjectTracking/Form1.Designer.cs
@@ -29,11 +29,13 @@
 		private void InitializeComponent()
 		{
 			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.videoSourcePlayer = new AForge.Controls.VideoSourcePlayer();
 			this.SuspendLayout();
 			// 
 			// button1
 			// 
-			this.button1.Location = new System.Drawing.Point(94, 86);
+			this.button1.Location = new System.Drawing.Point(22, 227);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(75, 23);
 			this.button1.TabIndex = 0;
@@ -41,11 +43,33 @@
 			this.button1.UseVisualStyleBackColor = true;
 			this.button1.Click += new System.EventHandler(this.Upload);
 			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(184, 227);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "View";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.View);
+			// 
+			// videoSourcePlayer1
+			// 
+			this.videoSourcePlayer.AutoSizeControl = true;
+			this.videoSourcePlayer.Location = new System.Drawing.Point(22, 13);
+			this.videoSourcePlayer.Name = "videoSourcePlayer1";
+			this.videoSourcePlayer.Size = new System.Drawing.Size(237, 208);
+			this.videoSourcePlayer.TabIndex = 2;
+			this.videoSourcePlayer.VideoSource = null;
+			this.videoSourcePlayer.NewFrame += new AForge.Controls.VideoSourcePlayer.NewFrameHandler(this.videoSourcePlayer_NewFrame);
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(284, 262);
+			this.Controls.Add(this.videoSourcePlayer);
+			this.Controls.Add(this.button2);
 			this.Controls.Add(this.button1);
 			this.Name = "Form1";
 			this.Text = "Form1";
@@ -56,6 +80,7 @@
 		#endregion
 
 		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private AForge.Controls.VideoSourcePlayer videoSourcePlayer;
 	}
 }
-
--- ObjectTracking/Form1.cs
+++ ObjectTracking/Form1.cs
@@ -1,16 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Windows.Forms;
+using AForge.Imaging;
+using AForge.Imaging.Filters;
+using AForge.Video;
+using AForge.Video.VFW;
 
 namespace ObjectTracking
 {
 	public partial class Form1 : Form
 	{
+		private List<Rectangle> BlobRects { get; set; }
+		private Bitmap PrevImage { get; set; }
+		private System.Timers.Timer Timer { get; set; }
+		 
 		public Form1()
 		{
 			InitializeComponent();
@@ -29,15 +34,13 @@ namespace ObjectTracking
 
 			// Intialize variables
 
-			string strDirectory = "C:/Users/Dominic Northey/Documents/Work/Notes/Year 6/Sem 1/Computer Vision";
-
 			// Open the dialog box
 
 			OpenFileDialog openFileDialog1 = new OpenFileDialog();
 
-			openFileDialog1.InitialDirectory = strDirectory;
+			openFileDialog1.InitialDirectory = "C:/Users/Dominic Northey/Videos/Sky Diving";
 
-			openFileDialog1.Filter = "Video Files (*.mp4, *.wma)|*.mp4, *.wma|All files (*.*)|*.*";
+			openFileDialog1.Filter = "Video Files (*.mp4, *.wma)|*.mp4; *.wma|All files (*.*)|*.*";
 			openFileDialog1.FilterIndex = 1;
 			openFileDialog1.Multiselect = false;
 			openFileDialog1.RestoreDirectory = true;
@@ -48,8 +51,132 @@ namespace ObjectTracking
 
 			if (Result == DialogResult.OK)
 			{
-				//openFileDialog1.FileName
+				 OpenVideoSource(new AVIFileVideoSource(openFileDialog1.FileName));
 			}
+		}
+
+		private void View(object sender, EventArgs e)
+		{
+			//if (Input == null) 
+			//{
+			//    MessageBox.Show("Please Upload a video.");
+			//    return;
+			//}
+
+			//ShowAllMotion();
+
+			//videoSourcePlayer.VideoSource = Input.;
+
+			//Input.Close();
+		}
+
+
+
+		private void videoSourcePlayer_NewFrame(object sender, ref Bitmap image)
+		{
+			//if (Timer. % motionRefeshCounter == 0)
+			{
+				videoSourcePlayer.GetCurrentVideoFrame();
+				BlobRects = GetLocationRectangles(ThresholdImage(PrevImage, image));
+				PrevImage = image;
+			}
+
+			if (BlobRects.Count == 0) continue;
+
+			DrawBlobs(BlobRects, image);
+		}
+
+		private Bitmap ThresholdImage(Bitmap prevImage, Bitmap image)
+		{
+			// Create filter
+			Subtract filter = new Subtract(prevImage);
+
+			// Apply the filter
+			Bitmap resultImage = filter.Apply(image);
+
+			new Threshold().ApplyInPlace(resultImage);
+			return resultImage;
+		}
+
+		private List<Rectangle> GetLocationRectangles(Bitmap thresholdImage)
+		{
+			BlobCounter blobCounter = new BlobCounter();
+
+			// Get object rectangles
+			blobCounter.ProcessImage(thresholdImage);
+			Rectangle[] rects = blobCounter.GetObjectsRectangles();
+						
+			List<Rectangle> largeBlobRects = new List<Rectangle>();
+
+			foreach ( Rectangle rc in rects )
+			{
+				if ( ( rc.Width < 35 ) && ( rc.Height < 35 ) ) continue;
+					
+				largeBlobRects.Add(rc);					
+			}
+
+			return largeBlobRects;
+		}
+
+		private void DrawBlobs(List<Rectangle> blobRects, Bitmap image)
+		{
+			// Create graphics object from initial image
+			Graphics g = Graphics.FromImage(image);
+
+			// Draw each rectangle
+			using (Pen pen = new Pen(Color.Red, 2))
+			{
+				foreach (Rectangle rc in blobRects)
+				{
+					g.DrawRectangle(pen, rc);
+				}
+			}
+
+			g.Dispose();
+		}
+		
+		// Open video source
+		private void OpenVideoSource(IVideoSource source)
+		{
+			// set busy cursor
+			this.Cursor = Cursors.WaitCursor;
+
+			// close previous video source
+			CloseVideoSource();
+
+			// start new video source
+			videoSourcePlayer.VideoSource = new AsyncVideoSource(source);
+			videoSourcePlayer.Start();
+			
+			Timer.Start();
+
+			Timer.Interval = 0.30;
+			//Timer.Elapsed();
+
+			this.Cursor = Cursors.Default;
+		}
+
+		// Close current video source
+		private void CloseVideoSource()
+		{
+			// set busy cursor
+			this.Cursor = Cursors.WaitCursor;
+
+			// stop current video source
+			videoSourcePlayer.SignalToStop();
+
+			// wait 2 seconds until camera stops
+			for (int i = 0; (i < 50) && (videoSourcePlayer.IsRunning); i++)
+			{
+				Thread.Sleep(100);
+			}
+			if (videoSourcePlayer.IsRunning)
+			{
+				videoSourcePlayer.Stop();
+			}
+
+			// stop timers
+			Timer.Stop();
 		}
 	}
 }
